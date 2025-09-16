3f820ffe6ab985afa4c88a8e139e38c003ae749b
kaelmoreno-cp <kael.moreno@coreproc.com>
2025-09-16 14:19:56 +0800

    Remove Greeting and Platform files
    This commit removes the `Greeting.kt` and `Platform.kt` files from the project.
    
    Key changes:
    - Deleted `Greeting.kt`, which contained the `Greeting` class used to generate platform-specific greetings.
    - Deleted `Platform.kt`, which defined the `Platform` interface and the `getPlatform()` expect function.

--- composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/data/network/NetworkClient.kt
+++ composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/data/network/NetworkClient.kt
@@ -11,11 +11,6 @@ import kotlinx.serialization.json.Json
 object NetworkClient {
     val httpClient = HttpClient {
 
-        headers {
-            append("Accept", "application/json")
-            append("User-Agent", "KMM-App/1.0")
-        }
-
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
