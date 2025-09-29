402e808f8a4c31c72cdc74f7626ebfc536905c8b
Cj Adonis <90882247+Jcjays@users.noreply.github.com>
2025-09-29 23:07:57 +0800

    Shortened initialization of koin dependencies

--- composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/di/DataModule.kt
@@ -9,27 +9,25 @@ import com.kaelmoreno.compose.composemultiplatformbase.data.network.ApiService
 import com.kaelmoreno.compose.composemultiplatformbase.data.repository.DataStoreRepository
 import com.kaelmoreno.compose.composemultiplatformbase.data.repository.EncryptedDataStoreRepository
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataModule = module {
 
     // DataStore - singleton for preferences storage
-    single<DataStore<Preferences>> { createDataStore() }
+    singleOf(::createDataStore)
 
     // Encryption Service - singleton for encrypting/decrypting data
-    single<EncryptionService> { createEncryptionService() }
+    singleOf(::createEncryptionService)
 
     // DataStoreRepository - singleton that gets DataStore injected (original, unencrypted)
-    single<DataStoreRepository> { DataStoreRepository(get()) }
+    singleOf(::DataStoreRepository)
 
     // EncryptedDataStoreRepository - singleton that gets DataStore and EncryptionService injected
-    single<EncryptedDataStoreRepository> { EncryptedDataStoreRepository(get(), get()) }
+    singleOf(::EncryptedDataStoreRepository)
 
     // ApiService - singleton that gets Platform injected
-    single<ApiService> { ApiService(
-        platform = get(),
-        encryptedDataStoreRepository = get()
-    ) }
+    singleOf(::ApiService)
 }
 
 // Platform module will be defined separately for each platform
--- composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/com/kaelmoreno/compose/composemultiplatformbase/di/PresentationModule.kt
@@ -4,11 +4,12 @@ import com.kaelmoreno.compose.composemultiplatformbase.presentation.screen.main_
 import com.kaelmoreno.compose.composemultiplatformbase.presentation.screen.post_list_screen.PostsViewModel
 import com.kaelmoreno.compose.composemultiplatformbase.presentation.screen.user_list_screen.UserViewModel
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     // ViewModels - scoped to the component lifecycle
-    viewModel { MainScreenViewModel(get()) }
-    viewModel { UserViewModel(get()) }
-    viewModel { PostsViewModel(get()) }
+    viewModelOf(::MainScreenViewModel)
+    viewModelOf(::UserViewModel)
+    viewModelOf(::PostsViewModel)
 }
